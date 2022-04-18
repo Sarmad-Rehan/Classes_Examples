bdfea2bc44f5af2de948b15644e1740f95b86459
Sarmad Rehan <sarmadrehan52@gmail.com>
2022-04-18 09:14:39 +0500

    Update

--- main.dart
+++ main.dart
@@ -4,4 +4,8 @@ void main(List<String> args) {
   Account account = Account();
   account.createAccount();
   account.showDetails();
+
+  Account account2 = Account();
+  account2.createAccount();
+  account2.showDetails();
 }
